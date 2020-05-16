ba7f245cab33b1c27b6ce7d8a68e360a2d9ae6d7
TBoileau <t-boileau@email.com>
2020-05-17 01:09:30 +0200

    feat(maze): remove item from case
    Remove picked up item in case

--- src/domain/maze/entity/case.py
+++ src/domain/maze/entity/case.py
@@ -5,10 +5,13 @@ from src.domain.maze.entity.item import Item
 from src.domain.maze.value_object.position import Position
 
 
-class Empty(Cell):
+class Case(Cell):
     def __init__(self, position: Position):
         super().__init__(position)
         self.item: Union[None, Item] = None
 
     def add_item(self, item: str):
         self.item = Item(item, self)
+
+    def remove_item(self):
+        self.item = None
