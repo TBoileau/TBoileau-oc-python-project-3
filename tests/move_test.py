03b6d2846561d184a30e6471050afb30235eab3f
TBoileau <t-boileau@email.com>
2020-05-17 01:07:03 +0200

    test(maze): test add items on maze
    Implement unit tests :
    * `test_if_number_of_items_is_not_equal_to_3`
    
    Update unit tests :
    * `test_if_maze_creation_is_successful`
    * `test_if_x_and_x_are_too_small`
    * `test_if_player_is_empty`
    * `test_if_player_has_only_space`
    * `test_if_enemy_has_only_space`
    * `test_if_player_has_only_space`
    * `test_if_failed_with_bad_direction`
    * `test_if_failed_with_wrong_direction`
    * `test_if_player_move_on_maze_to_the_end`

--- tests/move_test.py
+++ tests/move_test.py
@@ -1,5 +1,8 @@
+from typing import List
+
 import pytest
 
+from src.domain.maze.entity.case import Case
 from src.domain.maze.entity.maze import Maze
 from src.domain.maze.exception.bad_cell_exception import BadCellException
 from src.domain.maze.resolver.maze_resolver import MazeResolver
@@ -8,10 +11,18 @@ from src.domain.maze.value_object.direction import Direction
 
 def test_if_player_move_on_maze_to_the_end():
     maze: Maze = Maze(3, 3, "Mac Gyver", "Guard", ['ether', 'needle', 'wood'])
-    maze_resolver: MazeResolver = MazeResolver(maze).resolve()
-    for direction in maze_resolver.directions:
-        maze.player.move(direction)
-    assert maze.end == maze.player.cell
+    steps: List[Case, bool] = [
+        (maze.cases_with_items[0], True),
+        (maze.cases_with_items[1], True),
+        (maze.cases_with_items[2], True),
+        (maze.end, False)
+    ]
+    for step in steps:
+        maze_resolver: MazeResolver = MazeResolver(maze).resolve(*step)
+        for direction in maze_resolver.directions:
+            maze.player.move(direction)
+    assert maze.end == maze.player.case
+    assert len(maze.player.items) == 3
 
 
 def test_if_failed_with_wrong_direction():
