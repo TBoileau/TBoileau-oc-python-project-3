47a5f5c08a04454557ce7ce70edd6fa99f9fbe59
Thomas Boileau <t-boileau@email.com>
2020-05-16 22:14:11 +0200

    Resolve maze (#12)
    * test(maze): update player move
    
    Update unit test `test_if_player_move_on_maze_to_the_end` by using `maze_resolver.py`
    
    * feat(maze): resolve maze
    
    Implement `maze_resolver.py` to prevent a possible feature and unit test `test_if_player_move_on_maze_to_the_end`

--- tests/move_test.py
+++ tests/move_test.py
@@ -2,13 +2,16 @@ import pytest
 
 from src.domain.maze.entity.maze import Maze
 from src.domain.maze.exception.bad_cell_exception import BadCellException
+from src.domain.maze.resolver.maze_resolver import MazeResolver
 from src.domain.maze.value_object.direction import Direction
 
 
 def test_if_player_move_on_maze_to_the_end():
     maze: Maze = Maze(3, 3, "Player")
-    maze.player.move(Direction.RIGHT)
-    assert maze.start != maze.player.cell
+    maze_resolver: MazeResolver = MazeResolver(maze).resolve()
+    for direction in maze_resolver.directions:
+        maze.player.move(direction)
+    assert maze.end == maze.player.cell
 
 
 def test_if_failed_with_wrong_direction():
