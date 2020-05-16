133135decce4d5b4567dfd858ebc79519bd8b7ad
TBoileau <t-boileau@email.com>
2020-05-16 22:39:14 +0200

    feat(maze): add items on maze
    Implement `item.py` and add 3 items randomly on maze

--- src/domain/maze/entity/empty.py
+++ src/domain/maze/entity/empty.py
@@ -1,5 +1,14 @@
+from typing import Union
+
 from src.domain.maze.entity.cell import Cell
+from src.domain.maze.entity.item import Item
+from src.domain.maze.value_object.position import Position
 
 
 class Empty(Cell):
-    pass
+    def __init__(self, position: Position):
+        super().__init__(position)
+        self.item: Union[None, Item] = None
+
+    def add_item(self, item: str):
+        self.item = Item(item, self)
